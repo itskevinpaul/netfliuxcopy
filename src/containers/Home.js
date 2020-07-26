a93b28593aa1b0c6d19738b66266547c7ae94ffb
itskevinpaul <58891238+itskevinpaul@users.noreply.github.com>
2020-07-26 14:39:34 +0530

    kev

---
+++ src/containers/Home.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from "../axios-movies";
+
+import Navbar from './Navbar';
+import MainContent from './MainContent';
+import Movie from '../components/Movie/Movie';
+import Modal from '../components/UI/Modal';
+import MovieDetails from '../components/Movie/MovieDetails';
+
+class Home extends Component {
+  state = {
+    /** Toggles the modal when a movie is clicked. */
+    toggleModal: false,
+    /** Holds the movie information for a single movie. */
+    movieOverview: {},
+  }
+
+
+  /* Get the appropriate details for a specific movie that was clicked */
+  // selectMovieHandler = (movie) => {
+  //   this.setState({ toggleModal: true });
+
+  //   let url;
+  //   /** Make the appropriate API call to get the details for a single movie or tv show. */
+  //   if (movie.media_type === "movie") {
+  //     const movieId = movie.id;
+  //     url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`;
+
+  //   } else if (movie.media_type === "tv") {
+  //     const tvId = movie.id
+  //     url = `https://api.themoviedb.org/3/tv/${tvId}?api_key=${process.env.API_KEY}`;
+  //   }
+
+  //   axios.get(url)
+  //     .then(res => {
+  //       const movieData = res.data;
+  //       this.setState({ movieOverview: movieData });
+  //     }).catch(error => {
+  //       console.log(error);
+  //     });
+  // }
+
+  closeModal = () => {
+    this.setState({ toggleModal: false });
+  }
+
+  render() {
+    return (
+      <>
+        <div class="main-content">
+          <MainContent />
+        </div>
+        <Modal show={this.state.toggleModal}
+          modalClosed={this.closeModal}
+          movie={this.state.movieOverview}>
+          <MovieDetails movie={this.state.movieOverview} />
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './store/reducers';
+import promise from 'redux-promise';
+import '@babel/polyfill';
+
+import App from './containers/App';
+// Import main sass file to apply global styles
+import './static/sass/style.scss';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+// TODO
+// - fix styling issue
+// - implement carousel
+// - fix modal backdrop bug
+const app = (
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('app'));
